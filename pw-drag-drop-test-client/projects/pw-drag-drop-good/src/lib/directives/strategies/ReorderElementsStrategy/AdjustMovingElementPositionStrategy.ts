bfb7a86f6e3a7699b722d7242485436f341a422d
Test <piess105@gmail.com>
2021-09-06 20:40:12 +0200

    AdjustMovingElementPositionStrategy

--- pw-drag-drop-test-client/projects/pw-drag-drop-good/src/lib/directives/strategies/ReorderElementsStrategy/AdjustMovingElementPositionStrategy.ts
+++ pw-drag-drop-test-client/projects/pw-drag-drop-good/src/lib/directives/strategies/ReorderElementsStrategy/AdjustMovingElementPositionStrategy.ts
@@ -14,7 +14,7 @@ export class AdjustMovingElementPositionStrategy extends ReorderElementsOnMoving
 
     execute(model: ElementWithReference, movingElementStartingPosition: number): void {
 
-        if (this.isMovingElementOutOfItsParent_Or_haventChangedThePosition(model.element)) {
+        if (this.isMovingElementOutOfItsParent_Or_haventSwappedWithOtherElement(model.element)) {
 
             this.restoreMovingElementOriginalPosition(model.element);
         }
@@ -23,11 +23,11 @@ export class AdjustMovingElementPositionStrategy extends ReorderElementsOnMoving
         }
     }
 
-    private isMovingElementOutOfItsParent_Or_haventChangedThePosition = (movingElement: Element): boolean => {
+    private isMovingElementOutOfItsParent_Or_haventSwappedWithOtherElement = (movingElement: Element): boolean => {
 
         var newIndex = this.getMovingElementNewIndexValue(movingElement);
-        var originalIndex =  this.getElementIndex(movingElement);
-        
+        var originalIndex = this.getElementIndex(movingElement);
+
         if (newIndex == undefined || newIndex == originalIndex)
             return true;
 
@@ -35,14 +35,36 @@ export class AdjustMovingElementPositionStrategy extends ReorderElementsOnMoving
     }
 
     private restoreMovingElementOriginalPosition = (movingElement: Element) => {
-        console.log("RESTORING");
-
+        this.renderer.removeStyle(movingElement, "transform");
     }
 
     private setMovingElementPositionBasedOnItCurrentPosition = (movingElement: Element) => {
-        console.log("Seting new");
+
+        var index = this.getMovingElement_NewIndex_CurrentIndex_Difference(movingElement);
+
+        var transform = this.getElement_Y_TransformByItsIndex(movingElement, index);
+
+        this.renderer.setStyle(movingElement, "transform", `translateY(${transform}px)`);
+    }
+
+    private getMovingElement_NewIndex_CurrentIndex_Difference = (movingElement: Element) => {
 
 
+        var newIndex = this.getMovingElementNewIndexValue(movingElement);
+        var originalIndex = this.getElementIndex(movingElement);
+
+        var difference = newIndex! - originalIndex;
+
+        return difference;
+    }
+
+    private getElement_Y_TransformByItsIndex = (element: Element, index: number): number => {
+
+        var margin = parseInt(getComputedStyle(element).marginTop);
+
+        var transform = (element.getBoundingClientRect().height + margin) * index;
+
+        return transform
     }
 
 }
